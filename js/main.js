55c9e69ba84b924e3e5177489bc9c0061a758634
HappyEverydayBoom <gdy940423@gmail.com>
2018-05-30 21:13:17 +0800

    5/30 9:13

--- js/main.js
+++ js/main.js
@@ -38,79 +38,79 @@ function show() {
         } else if (xY == "y") {
             return ((squareProportion[0] * (squareProportion[3] + 1)) + (squareProportion[1] * squareProportion[3]));
         } else if (xY == "s1") {
-            return squareProportion[0];
-        } else if (xY == "s2") {
-            return squareProportion[1];
-        } else if (xY == "h/y") {
-            return $(window).height() / ((squareProportion[0] * (squareProportion[3] + 1)) + (squareProportion[1] * squareProportion[3]));
-        } else {
-            return 0;
+            //         return squareProportion[0];
+            //     } else if (xY == "s2") {
+            //         return squareProportion[1];
+            //     } else if (xY == "h/y") {
+            //         return $(window).height() / ((squareProportion[0] * (squareProportion[3] + 1)) + (squareProportion[1] * squareProportion[3]));
+            //     } else {
+            //         return 0;
+            //     }
+            // }
+            // // console.log(squareProportion("x"));
+            // // console.log(squareProportion("y"));
+            // // console.log(squareProportion("s1"));
+            // // console.log(squareProportion("s2"));
+            // // console.log(squareProportion("h/y"));
+            // // console.log(squareProportion("a"));
+
+
+            // $("document").ready(function () {
+            //     height = $(window).height();
+            //     width = (squareProportion("h/y") * squareProportion("x"));
+            //     $(window).resize(function () {
+            //         height = $(window).height();
+            //         width = (squareProportion("h/y") * squareProportion("x"));
+            //         $("canvas").attr("height", height);
+            //         $("canvas").attr("width", width);
+            //     });
+            //     $("canvas").attr("height", height);
+            //     $("canvas").attr("width", width);
+            // });
+
+            // var canvas = document.getElementById("myCanvas");
+            // var FPS = 60;
+            // var ctx = canvas.getContext("2d");
+
+            // var light = { brightness: 80, status: "Brighten" };　//"darken"
+            // function drow() {
+            //     ctx.fillStyle = ("#00" + (Math.floor(light.brightness)).toString(16) + (Math.floor(light.brightness) + 30).toString(16));
+            //     var lightSpeed = 6.5;
+            //     if (light.status == "darken") {//一秒鐘變一次，變ＦＰＳ分之一次
+            //         if (light.brightness < 71) {
+            //             light.status = "Brighten";
+            //             light.brightness += lightSpeed / FPS;
+            //         } else {
+            //             light.brightness -= lightSpeed / FPS;
+            //         }
+            //     } else {
+            //         if (light.brightness > 99) {
+            //             light.status = "darken";
+            //             light.brightness -= lightSpeed / FPS;
+            //         } else {
+            //             light.brightness += lightSpeed / FPS;
+            //         }
+            //     }
+            //     // console.log(light.brightness);
+            //     ctx.fillRect(0, 0, width, height);
+
+            //     for (i = 0; i < squares.length; i++) {
+            //         for (l = 0; l < squares[i].length; l++) {
+            //             ctx.fillStyle = squares[i][l].getColor();
+            //             ctx.fillRect(
+            //                 ((i + 1) * squareProportion("s1") * (squareProportion("h/y")) + i * squareProportion("s2") * (squareProportion("h/y"))),
+            //                 ((l + 1) * squareProportion("s1") * (squareProportion("h/y")) + l * squareProportion("s2") * (squareProportion("h/y"))),
+            //                 (squareProportion("s2") * (squareProportion("h/y"))),
+            //                 (squareProportion("s2") * (squareProportion("h/y"))));
+            //         }
+            //     }
+        }
+
+        setInterval(drow, 1000 / FPS);
+    }
+
+    function play() {
+        var tetrises = {
+
         }
     }
-    // console.log(squareProportion("x"));
-    // console.log(squareProportion("y"));
-    // console.log(squareProportion("s1"));
-    // console.log(squareProportion("s2"));
-    // console.log(squareProportion("h/y"));
-    // console.log(squareProportion("a"));
-
-
-    $("document").ready(function () {
-        height = $(window).height();
-        width = (squareProportion("h/y") * squareProportion("x"));
-        $(window).resize(function () {
-            height = $(window).height();
-            width = (squareProportion("h/y") * squareProportion("x"));
-            $("canvas").attr("height", height);
-            $("canvas").attr("width", width);
-        });
-        $("canvas").attr("height", height);
-        $("canvas").attr("width", width);
-    });
-
-    var canvas = document.getElementById("myCanvas");
-    var FPS = 60;
-    var ctx = canvas.getContext("2d");
-
-    var light = { brightness: 80, status: "Brighten" };　//"darken"
-    function drow() {
-        ctx.fillStyle = ("#00" + (Math.floor(light.brightness)).toString(16) + (Math.floor(light.brightness) + 30).toString(16));
-        var lightSpeed = 6.5;
-        if (light.status == "darken") {//一秒鐘變一次，變ＦＰＳ分之一次
-            if (light.brightness < 71) {
-                light.status = "Brighten";
-                light.brightness += lightSpeed / FPS;
-            } else {
-                light.brightness -= lightSpeed / FPS;
-            }
-        } else {
-            if (light.brightness > 99) {
-                light.status = "darken";
-                light.brightness -= lightSpeed / FPS;
-            } else {
-                light.brightness += lightSpeed / FPS;
-            }
-        }
-        // console.log(light.brightness);
-        ctx.fillRect(0, 0, width, height);
-
-        for (i = 0; i < squares.length; i++) {
-            for (l = 0; l < squares[i].length; l++) {
-                ctx.fillStyle = squares[i][l].getColor();
-                ctx.fillRect(
-                    ((i + 1) * squareProportion("s1") * (squareProportion("h/y")) + i * squareProportion("s2") * (squareProportion("h/y"))),
-                    ((l + 1) * squareProportion("s1") * (squareProportion("h/y")) + l * squareProportion("s2") * (squareProportion("h/y"))),
-                    (squareProportion("s2") * (squareProportion("h/y"))),
-                    (squareProportion("s2") * (squareProportion("h/y"))));
-            }
-        }
-    }
-
-    setInterval(drow, 1000 / FPS);
-}
-
-function play() {
-    var tetrises = {
-
-    }
-}
